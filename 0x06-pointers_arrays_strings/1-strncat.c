126b19b684058f3a5536b30ae1db18ab6242db07
Judith Okumu <judith10akoth@gmail.com>
2023-03-30 12:30:32 -0700

    reverse array

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 
 /**
- * *_strncat - concatenates two strings
- * @dest: destination string
- * @src: source string
- * @n: number of characters to be append
+ * *_strncat - concatenates two strings using at most
+ * @dest: the string to be appended upon
+ * @src: the string to be appended to dest
+ * @n: the number of bytes from src to be appended to dest
  *
- * Return: concatenated string
+ * Return: a pointer to the resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, int dest_len = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[index++]
+	while (dest[index++])
 		dest_len++;
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
